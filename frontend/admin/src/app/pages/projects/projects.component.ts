1ce88eaa97da6cad667b236c13c12a0bdf6b2d40
OlegNau <70605440+OlegNau@users.noreply.github.com>
2025-09-02 23:20:50 +0300

    feat: scaffold angular app skeleton

---
+++ frontend/admin/src/app/pages/projects/projects.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProjectsComponent {}
