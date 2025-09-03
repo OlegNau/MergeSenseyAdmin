fa5a3f9f7f8bc344e364c5d948643ecd6cfdd342
o.p.naumov <o.p.naumov@gmail.com>
2025-09-03 18:50:04 +0300

    fix tailwind

--- frontend/admin/tailwind.config.js
+++ frontend/admin/tailwind.config.js
@@ -1,1 +1,7 @@
-module.exports = { darkMode: 'class', content: ['./src/**/*.{html,ts}'], theme:{ extend:{} }, plugins:[] };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: ['./src/**/*.{html,ts}'],
+    theme: { extend: {} },
+    plugins: [],
+};
--- frontend/admin/tailwind.v4.config.js
+++
@@ -1,9 +0,0 @@
-/** Tailwind v4 config */
-module.exports = {
-  darkMode: 'class',
-  content: ["./src/**/*.{html,ts}"],
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
-};
