acd586d12c5103a4a4e32b68008aea66be040379
OlegNau <70605440+OlegNau@users.noreply.github.com>
2025-09-08 09:25:51 +0300

    chore: configure dev issuer and cors

--- frontend/admin/src/environments/environment.prod.ts
+++ frontend/admin/src/environments/environment.prod.ts
@@ -1,25 +1,20 @@
 import { Environment } from '@abp/ng.core';
 
 const baseUrl = window.location.origin;
-const oAuthConfig = {
-  issuer: 'https://localhost:44396/',
-  redirectUri: baseUrl,
-  clientId: 'MergeSensei_App',
-  responseType: 'code',
-  scope: 'offline_access openid profile MergeSensei',
-  requireHttps: true,
-};
 
 export const environment = {
   production: true,
   application: { baseUrl, name: 'MergeSenseyAdmin' },
-  oAuthConfig,
+  oAuthConfig: {
+    issuer: 'https://localhost:44396/',
+    redirectUri: baseUrl,
+    clientId: 'MergeSensei_App',
+    responseType: 'code',
+    scope: 'offline_access openid profile MergeSensei',
+    requireHttps: true,
+  },
   apis: {
-    default: {
-      url: 'https://localhost:44396',
-    },
-    Default: {
-      url: 'https://localhost:44396',
-    },
+    default: { url: 'https://localhost:44396' },
+    Default: { url: 'https://localhost:44396' },
   },
 } as Environment;
